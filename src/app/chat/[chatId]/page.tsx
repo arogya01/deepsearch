935c8787ad4d4641638faf5902995750fed05599
arogya <arogyabichpuria16@gmail.com>
2025-10-20 00:00:06 +0530

    added Drizzle ORM practice exercises for hands-on query training

--- src/app/chat/[chatId]/page.tsx
+++ src/app/chat/[chatId]/page.tsx
@@ -1,7 +1,12 @@
+import { fetchChatMessages } from "../../actions/chat";
 import { ChatWindow } from "../../components/chat-window";
 
+export default async function ChatPage({
+  params,
+}: {
+  params: { chatId: string };
+}) {
+  const messages = await fetchChatMessages(params.chatId);
 
-export default function ChatPage({ params }: { params: { chatId: string } }) {
-  return <ChatWindow chatId={params.chatId} />
+  return <ChatWindow chatId={params.chatId} initialMessages={messages} />;
 }
-
