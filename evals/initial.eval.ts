b3e1ef1499648d65c55aebcb4e4c6bb069e624d1
arogya <arogyabichpuria16@gmail.com>
2025-11-15 18:49:48 +0530

    setup evals

--- evals/initial.eval.ts
+++ evals/initial.eval.ts
@@ -1,6 +1,14 @@
 import {evalite} from "evalite";
 import {Levenshtein} from "autoevals";
 
-export const initialEval = evalite({
-    
+export const initialEval = evalite("My Eval",{
+    data: async () => {
+        return [
+            {input: "hello", expected: "hello world"}
+        ]
+    }, 
+    task: async (input) => {
+        return input + " world";
+    },
+   scorers: [Levenshtein]
 })
