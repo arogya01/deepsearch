5465be2ea6aa9ca5671bf09a68290c9590bc6412
arogya <arogyabichpuria16@gmail.com>
2025-11-29 18:24:10 +0530

    built the baseline for system context

--- src/system-context.ts
+++ src/system-context.ts
@@ -1,14 +1,42 @@
-import type {Message} from "ai"; 
+
+
+type QuerySearchResult = {
+    date: string; 
+    title: string; 
+    url: string; 
+    snippet: string; 
+}; 
+
+type QueryResult = {
+    query: string; 
+    result: QuerySearchResult[]; 
+}
+
+type ScrapeResult = {
+    url:string; 
+    result: string; 
+}
+
 
 export class SystemContext{
-    // private variables: 
-    step: number; 
-    queries: string[]; 
-    url: string;
-    constructor({step,queries, url }: {step: number, queries: string[], url: string}){
-        this.step = step;
-        this.queries = queries;
-        this.url = url;
+    // the current step in the loop 
+    private step = 0; 
+    // the history of all queries searched
+    private queryHistory: QueryResult[] = []; 
+    // the history of all URL's scraped
+    private scrapeHistory: ScrapeResult[] = [];     
+    
+
+    shouldStop(){
+        return this.step >= 10; 
+    }
+
+    reportQueries(queries: QueryResult[]){
+        return this.queryHistory.push(...queries); 
+    }
+
+    reportScrapes(scrapes: ScrapeResult[]){
+        return this.scrapeHistory.push(...scrapes); 
     }
 
 }
