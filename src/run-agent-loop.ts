a26090dffd5e469ea2cee619f3b9ccd19d382925
arogya <arogyabichpuria16@gmail.com>
2025-12-05 15:29:27 +0530

    constructing the main agent loop

--- src/run-agent-loop.ts
+++ src/run-agent-loop.ts
@@ -2,6 +2,9 @@
 
 import { SystemContext } from "./system-context";
 import { streamDeepSearch } from "./server/search/ai-sdk";
+import { performWebSearch } from "./server/search/web-search";
+import { performWebScrape } from "./server/search/web-scraper";
+import { getNextAction } from "./get-next-action";
 
 
 export async function runAgentLoop(question: string) {
@@ -11,7 +14,13 @@ export async function runAgentLoop(question: string) {
 
   while(!ctx.shouldStop()){
       ctx.incrementStep();
+
+    const nextAction = getNextAction(ctx); 
+
+    
+
   }
+
 
   return "Agent loop completed, but no answer generated yet.";
 }
